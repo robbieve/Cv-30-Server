ba4844a2791dc1dc8edc5a9918991887aa68f4ee
robbieve <robbieeve751994@gmail.com>
2018-06-20 13:49:39 +0300

    update to setContext

--- app/context.js
+++ app/context.js
@@ -17,46 +17,47 @@ module.exports = async ({headers, cookies}) => {
     let authorization = null;
     let refreshToken = null;
     let user = null;
+    let bearerLength = 0;
     if (cookies[process.env.TOKEN_NAME]) authorization = cookies[process.env.TOKEN_NAME];
     if (cookies[process.env.REFRESH_TOKEN_NAME]) refreshToken = cookies[process.env.REFRESH_TOKEN_NAME];
-    if (!authorization && headers[process.env.TOKEN_NAME]) authorization = headers[process.env.TOKEN_NAME];
+    if (!authorization && headers[process.env.TOKEN_NAME]) {
+        authorization = headers[process.env.TOKEN_NAME];
+        bearerLength = "Bearer ".length;
+    }
     if (!refreshToken && headers[process.env.REFRESH_TOKEN_NAME]) refreshToken = headers[process.env.REFRESH_TOKEN_NAME];
-    if (authorization) {
-        const bearerLength = "Bearer ".length;
-        if (authorization && authorization.length > bearerLength) {
-            const token = authorization.slice(bearerLength);
-            const decodedToken = await jwt.decode(token);
-            if (decodedToken) {
-                decodedToken.data = JSON.parse(_decrypt(decodedToken.data));
-                user = await models.user.findOne({ where: {id: decodedToken.data.id} });
-            }
-            if (user) {
-                const { ok, result } = await new Promise(function(resolve) {
-                    jwt.verify(token, user.salt + process.env.JWT_SECRET, function(err, result) {
-                        if (err) {
-                            resolve({
-                                ok: false,
-                                result: err
-                            });
-                        } else {
-                            resolve({
-                                ok: true,
-                                result
-                            });
-                        }
-                    });
+    if (authorization && authorization.length > bearerLength) {
+        const token = authorization.slice(bearerLength);
+        const decodedToken = await jwt.decode(token);
+        if (decodedToken) {
+            decodedToken.data = JSON.parse(_decrypt(decodedToken.data));
+            user = await models.user.findOne({ where: {id: decodedToken.data.id} });
+        }
+        if (user) {
+            const { ok, result } = await new Promise(function(resolve) {
+                jwt.verify(token, user.salt + process.env.JWT_SECRET, function(err, result) {
+                    if (err) {
+                        resolve({
+                            ok: false,
+                            result: err
+                        });
+                    } else {
+                        resolve({
+                            ok: true,
+                            result
+                        });
+                    }
                 });
-                if (ok) {
-                    return {
-                        user,
-                        models
-                    };
-                } else {
-                    return {
-                        user: null,
-                        models
-                    };
-                }
+            });
+            if (ok) {
+                return {
+                    user,
+                    models
+                };
+            } else {
+                return {
+                    user: null,
+                    models
+                };
             }
         }
     }
