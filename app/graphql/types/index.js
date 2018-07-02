66d387da0cac1e19709df148c9553a0b9250430a
robbieve <robbieeve751994@gmail.com>
2018-07-02 12:04:46 +0300

    handleCompany details required

--- app/graphql/types/index.js
+++ app/graphql/types/index.js
@@ -98,7 +98,7 @@ const Query = `
 		): StandardResponse
 		handleCompany(
 			language: LanguageCodeType!
-			details: CompanyInput
+			details: CompanyInput!
 		): StandardResponse
 		handleArticle (
 			language: LanguageCodeType!
