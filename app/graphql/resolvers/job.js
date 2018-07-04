daff78714b570ace9d242a6b6c034fccc3925fb3
robbieve <robbieeve751994@gmail.com>
2018-07-04 15:25:29 +0300

    Job and Team resolvers id,company,user checks

--- app/graphql/resolvers/job.js
+++ app/graphql/resolvers/job.js
@@ -1,6 +1,6 @@
 const uuid = require('uuidv4');
 const schema = require('../validation');
-const { checkUserAuth, yupValidation } = require('./common');
+const { checkUserAuth, yupValidation, throwForbiddenError } = require('./common');
 
 const handleJob = async (language, jobDetails, { user, models }) => {
     checkUserAuth(user);
@@ -15,16 +15,23 @@ const handleJob = async (language, jobDetails, { user, models }) => {
         }
     });
 
-    if (jobDetails) {
-        await models.sequelize.transaction(async t => {
+    const company = await models.company.findOne({ where: { id: jobDetails.companyId } });
+    if (!company)
+        return { status: false, error: 'Company not found' };
+    
+    if (company.userId != user.id)
+        throwForbiddenError();
 
-            jobDetails.id = jobDetails.id || uuid();
-            await models.job.upsert(jobDetails, { transaction: t });
-            jobDetails.jobId = jobDetails.id;
-            jobDetails.languageId = language.id;
-            await models.jobText.upsert(jobDetails, { transaction: t });
-        });
-    }
+    if (jobDetails.id && !await models.job.findOne({ where: { id: jobDetails.id } }))
+        return { status: false, error: 'Job not found' }
+
+    await models.sequelize.transaction(async t => {
+        jobDetails.id = jobDetails.id || uuid();
+        await models.job.upsert(jobDetails, { transaction: t });
+        jobDetails.jobId = jobDetails.id;
+        jobDetails.languageId = language.id;
+        await models.jobText.upsert(jobDetails, { transaction: t });
+    });
 
     return { status: true };
 }
--- app/graphql/resolvers/team.js
+++ app/graphql/resolvers/team.js
@@ -1,10 +1,20 @@
 const uuid = require('uuidv4');
 const schema = require('../validation');
-const { checkUserAuth, yupValidation } = require('./common');
+const { checkUserAuth, yupValidation, throwForbiddenError } = require('./common');
 
 const handleTeam = async (teamDetails, { user, models }) => {
     checkUserAuth(user);
     yupValidation(schema.team.input, { teamDetails });
+
+    const company = await models.company.findOne({ where: { id: teamDetails.companyId } });
+    if (!company)
+        return { status: false, error: 'Company not found' };
+    
+    if (company.userId != user.id)
+        throwForbiddenError();
+
+    if (teamDetails.id && !await models.team.findOne({ where: { id: teamDetails.id } }))
+        return { status: false, error: 'Team not found' }
 
     teamDetails.id = teamDetails.id || uuid();
     teamDetails.coverBackground = teamDetails.profileBackgroundColor;
@@ -18,7 +28,17 @@ const addMemberToTeam = async (teamId, memberId, { user, models }) => {
     yupValidation(schema.team.addRemoveMemberInput, { teamId, memberId });
 
     const team = await models.team.findOne({ where: { id: teamId } });
+    if (!team)
+        return { status: false, error: 'Team not found' };
+
+    const company = await models.company.findOne({ where: { id: team.companyId } });
+    if (!company || company.userId != user.id)
+        throwForbiddenError();
+
     const member = await models.user.findOne({ where: { id: memberId } });
+    if (!member)
+        return { status: false, error: 'Member not found' };
+
     team.addMember(member);
 
     return { status: true };
@@ -29,7 +49,17 @@ const removeMemberFromTeam = async (teamId, memberId, { user, models }) => {
     yupValidation(schema.team.addRemoveMemberInput, { teamId, memberId });
 
     const team = await models.team.findOne({ where: { id: teamId } });
+    if (!team)
+        return { status: false, error: 'Team not found' };
+
+    const company = await models.company.findOne({ where: { id: team.companyId } });
+    if (!company || company.userId != user.id)
+        throwForbiddenError();
+
     const member = await models.user.findOne({ where: { id: memberId } });
+    if (!member)
+        return { status: false, error: 'Member not found' };
+
     team.removeMember(member);
 
     return { status: true };
