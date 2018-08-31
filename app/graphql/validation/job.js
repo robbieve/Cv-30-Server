7f4d7aac7c9d666b98ac1df019de1e7a39a86999
robbieve <robbieeve751994@gmail.com>
2018-08-31 15:31:52 +0300

    added job media validation

--- app/graphql/validation/job.js
+++ app/graphql/validation/job.js
@@ -35,6 +35,8 @@ module.exports = {
                 isPublic: yup.boolean().required()
             }),
             activityField: yup.string().trim().max(255),
+            imagePath: yup.string().trim(),
+            videoUrl: yup.string().trim(),
             skills: yup.array().of(
                 yup.string().trim()
                     .required('Skill needs a title')
